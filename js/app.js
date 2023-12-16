0d90fe5faf4ebf59116aedc2eecfed4a4766197a
Josep Ocaña <j.ocanapuigdevall@gmail.com>
2023-12-16 17:20:44 +0100

    saludo javascript agregado

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-console.log('hola mundo');
+const btn = document.querySelector('.btn');
+function saludar() {
+	btn.textContent = 'Boton apretado';
+	setTimeout(() => {
+		alert('Hola desde Javascript');
+	}, 1000);
+}
